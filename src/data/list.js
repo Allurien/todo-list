c16187943be9e773a18d959affd254452c7f9d2b
Steffany <kielen@gmail.com>
2018-06-20 16:12:42 -0700

    added dummy data and displayed it

---
+++ src/data/list.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Eat Dinner',
+        details: 'Possibly chicken',
+        id: '001'
+    },
+    {
+        title: 'Kiss Boy',
+        details: 'Hi Ryan ;)',
+        id: '002'
+    },
+    {
+        title: 'Farm',
+        details: 'Make it to winter!',
+        id: '003'
+    },
+    {
+        title: 'Sleep',
+        details: 'No one does this...',
+        id: '004'
+    },
+];
